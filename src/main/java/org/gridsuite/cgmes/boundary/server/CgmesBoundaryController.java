82fc87e7ed821cddbdead425cd9df3efd9fc397d
Franck LECUYER <franck.lecuyer@rte-france.com>
2021-04-21 14:39:51 +0200

    Modifications after code review
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/cgmes/boundary/server/CgmesBoundaryController.java
+++ src/main/java/org/gridsuite/cgmes/boundary/server/CgmesBoundaryController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -96,10 +97,7 @@ public class CgmesBoundaryController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "The list of all available tsos")})
     public ResponseEntity<Set<String>> getTsos() {
         Optional<Set<String>> tsos = cgmesBoundaryService.getTsos();
-        if (!tsos.isPresent()) {
-            throw new PowsyblException("No available list of tsos");
-        }
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(tsos.get());
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(tsos.orElse(Collections.emptySet()));
     }
 
     @PostMapping(value = "/tsos")
@@ -114,10 +112,7 @@ public class CgmesBoundaryController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "The list of all available business processes")})
     public ResponseEntity<Set<String>> getBusinessProcesses() {
         Optional<Set<String>> businessProcesses = cgmesBoundaryService.getBusinessProcesses();
-        if (!businessProcesses.isPresent()) {
-            throw new PowsyblException("No available list of business processes");
-        }
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(businessProcesses.get());
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(businessProcesses.orElse(Collections.emptySet()));
     }
 
     @PostMapping(value = "/business-processes")
