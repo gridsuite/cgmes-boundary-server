8659da1a8e7588b4552918d32abd6f2fd1689210
AbdelHedhili <abdelsalem.hedhili@rte-france.com>
2021-05-10 13:02:58 +0200

    springboot 2.4
    Signed-off-by: AbdelHedhili <abdelsalem.hedhili@rte-france.com>

--- src/main/java/org/gridsuite/cgmes/boundary/server/CassandraConfig.java
+++ src/main/java/org/gridsuite/cgmes/boundary/server/CassandraConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 /**
@@ -32,10 +32,12 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     }
 
     @Bean
-    public CassandraClusterFactoryBean cluster(Environment env) {
-        CassandraClusterFactoryBean clusterFactory = new CassandraClusterFactoryBean();
-        clusterFactory.setContactPoints(env.getRequiredProperty("cassandra.contact-points"));
-        clusterFactory.setPort(Integer.parseInt(env.getRequiredProperty("cassandra.port")));
-        return clusterFactory;
+    public CqlSessionFactoryBean cassandraSession(Environment env) {
+        CqlSessionFactoryBean session = new CqlSessionFactoryBean();
+        session.setContactPoints(env.getRequiredProperty("cassandra.contact-points"));
+        session.setPort(Integer.parseInt(env.getRequiredProperty("cassandra.port")));
+        session.setLocalDatacenter("datacenter1");
+        session.setKeyspaceName(getKeyspaceName());
+        return session;
     }
 }
